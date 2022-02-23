f7c7093730d187255b7a4d72c05818b44b528555
Danny Xu <danny.d.xu@gmail.com>
2022-02-23 10:20:24 -0600

    Initial Commit

---
+++ CashRegisterApp/CashRegisterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashRegisterAppApp.swift
+//  CashRegisterApp
+//
+//  Created by 90308209 on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CashRegisterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
